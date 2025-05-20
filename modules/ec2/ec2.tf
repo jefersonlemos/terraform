fa14260ae44aaaa85274b95c64c2f7bad73caefa
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2025-05-20 11:39:34 -0300

    feat(ec2): Fixes the EC2 module

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -3,16 +3,17 @@ module "ec2_instance" {
 
   for_each = var.ec2_instances
 
-  name          = "instance-${each.key}"
+  name          = "${each.key}"
   ami           = each.value.ami
   instance_type = each.value.instance_type
   #TODO - It should create the key pair if it does not exist and if enabled by the user
   key_name   = each.value.key_name
   monitoring = each.value.monitoring
   #TODO - This module can create a SG or apply an existing one if the user set it
-  vpc_security_group_ids = each.value.security_groups
+  vpc_security_group_ids = each.value.vpc_security_group_ids
   subnet_id = each.value.subnet_id
 
+  #TODO - Fix it to be possible to add extra tags
   # extra_tags = {
   #   Terraform   = "true"
   #   Environment = "dev"
