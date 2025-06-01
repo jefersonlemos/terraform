ad22ee1177dc46170bd2b16580e738d3af107d20
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2025-06-01 17:51:40 -0300

    Changes worker size

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -40,7 +40,7 @@ module "eks" {
       max_size     = 2
       desired_size = 1
 
-      instance_types = ["t3.large"]
+      instance_types = ["t3.medium"]
       capacity_type  = "SPOT"
     
       iam_role_additional_policies = {
