7562f449d2d91c79cd7df4c59c0d912c190fcfa4
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2025-06-30 10:36:30 -0300

    feat(ec2): Add outputs for EC2 instance IDs and private DNS

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,1 +1,17 @@
 #TODO - Outputs -> EC2 name, sg id, subnet
+
+output "ec2_instance_ids" {
+  description = "IDs of all EC2 instances"
+  value = {
+    for k, inst in module.ec2_instance :
+    k => inst.id
+  }
+}
+
+output "ec2_instance_private_dns" {
+  description = "Private DNS of all EC2 instances"
+  value = {
+    for k, inst in module.ec2_instance :
+    k => inst.private_dns
+  }
+}
